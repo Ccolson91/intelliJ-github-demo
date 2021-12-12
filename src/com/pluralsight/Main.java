69b0557fbd6c834cdaa3df9d3c9aa0308bc261a6
Christian Colson <christiancolson91@gmail.com>
2021-12-12 09:56:52 -0600

    Fake John Dev pushing from his branch

--- src/com/pluralsight/Main.java
+++ src/com/pluralsight/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	     System.out.println("Hello IntelliJ CE");
+         System.out.println("This is John, working from my own branch!");
     }
 }
